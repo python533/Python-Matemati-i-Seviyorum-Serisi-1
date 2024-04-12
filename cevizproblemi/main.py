97876afb53b7bad8a6d9ab554519b7d8444faf0d
python533 <143389231+python533@users.noreply.github.com>
2024-04-13 00:57:02 +0300

    Python Cevizleri Cuvala Koy
    500 cevizi 10' adet  cuvala koymak istersen her bir cuvalda kac ceviz olur?

---
+++ cevizproblemi/main.py
@@ -0,0 +1,42 @@
+import random
+
+cuval1=[]
+cuval2=[]
+cuval3=[]
+cuval4=[]
+cuval5=[]
+cuval6=[]
+cuval7=[]
+cuval8=[]
+cuval9=[]
+cuval10=[]
+
+for i in range(0,501):
+    if i == 50:
+        cuval1.append(50)
+    elif i == 100:
+        cuval2.append(100)
+    elif i == 150:
+        cuval3.append(150)
+    elif i == 200:
+        cuval4.append(200)
+    elif i == 250:
+        cuval5.append(250)
+    elif i == 300:
+        cuval6.append(300)
+    elif i == 350:
+        cuval7.append(350)
+    elif i == 400:
+        cuval8.append(400)
+    elif i == 450:
+        cuval9.append(450)
+    elif i == 500:
+        cuval10.append(500)
+    else:
+        print("Dimyat'a pirince giderken evdeki bulgurdan olmayın!")
+
+
+
+
+
+
